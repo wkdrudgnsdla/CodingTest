3a5c3e4860d301f58204a2d8ec057ab2f0aaa323
Jang Gyeong Hun <s24032@gsm.hs.kr>
2025-09-24 10:34:52 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.03 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.py
@@ -0,0 +1,8 @@
+def solution(my_string, k):
+    result = ""
+    result = my_string * k
+    print('"', end="")
+    print(result, end="")
+    print('"',end="")
+    answer = result
+    return answer
